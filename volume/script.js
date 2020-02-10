5d9432079519f37a1c1c1e8160a718bceb4489b9
GlebUliv <55991627+GlebUliv@users.noreply.github.com>
2020-02-10 15:42:41 +0200

    Add files via upload

--- volume/script.js
+++ volume/script.js
@@ -1,14 +1,19 @@
 let volume = 50
 function render() {
     // можно таким образом:
-    let screen = document.getElementById('screen')
+    let screen = document.getElementById('screen');
     // let screen = document.getElementById('player').firstElementChild;
-    let slider = document.getElementById('slider')
+    let slider = document.getElementById('slider');
     // let slider = document.getElementById('player').children[1].children[1];
     screen.value = volume
     slider.value = volume
 
-    
+    screen.addEventListener('input', () =>
+        slider.value = screen.value
+    )
+    slider.addEventListener('input', () =>
+        screen.value = slider.value
+    )
     
 }
 
